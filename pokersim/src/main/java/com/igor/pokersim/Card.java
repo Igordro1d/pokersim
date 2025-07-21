f9d4442ba78f06b8666d8c251e4d4843dd8dbe83
Igordro1d <igormoreiracastro0@gmail.com>
2025-07-21 23:23:38 +1200

    Add Card, Rank, and Suit classes for poker simulation

---
+++ pokersim/src/main/java/com/igor/pokersim/Card.java
@@ -0,0 +1,16 @@
+package com.igor.pokersim;
+
+public class Card {
+    private final Rank rank;
+    private final Suit suit;
+    public Card(Rank rank, Suit suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+    public Rank getRank() {
+        return rank;
+    }
+    public Suit getSuit() {
+        return suit;
+    }
+}
---
+++ pokersim/src/main/java/com/igor/pokersim/Suit.java
@@ -0,0 +1,5 @@
+package com.igor.pokersim;
+
+public enum Suit {
+    CLUBS, DIAMONDS, HEARTS, SPADES
+}
